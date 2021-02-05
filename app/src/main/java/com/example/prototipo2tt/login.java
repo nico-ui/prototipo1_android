b9ae5700d34a94c815a4a4f178640032efae189b
Nicolai Hernández <cristian.nicolai@outlook.es>
2021-02-04 21:55:41 -0600

    segundo commit - alumno_home añadido

--- app/src/main/java/com/example/prototipo2tt/login.java
+++ app/src/main/java/com/example/prototipo2tt/login.java
@@ -8,13 +8,14 @@ import android.widget.Button;
 
 public class login extends Activity {
 
-    Button jbtn_reg;
-    Intent itn;
+    Button jbtn_reg, jbtn_ing;
+    Intent itn, itn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_alumno);
         jbtn_reg = (Button) findViewById(R.id.btn_registrar);
+        jbtn_ing = (Button) findViewById(R.id.btn_ingresar);
 
         jbtn_reg.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
@@ -22,6 +23,16 @@ public class login extends Activity {
                 startActivity(itn);
             }
         });
+
+
+        jbtn_ing.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                itn2 = new Intent(login.this, alumno_home.class);
+                startActivity(itn2);
+            }
+        });
+
+
     }
 
 }
