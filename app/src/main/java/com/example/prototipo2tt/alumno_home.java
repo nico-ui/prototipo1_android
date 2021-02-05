7f2c263fc3994e990f4470575ca8ef9a58be5e46
Nicolai Hernández <cristian.nicolai@outlook.es>
2021-02-04 22:00:20 -0600

    tercer commit - archivos adicionales alumno_home

---
+++ app/src/main/java/com/example/prototipo2tt/alumno_home.java
@@ -0,0 +1,14 @@
+package com.example.prototipo2tt;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class alumno_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alumno_home);
+
+    }
+}
