26ea4e9fd6442696d7ad1122bfbd0de48bb7cc52
Nicolai Hernández <cristian.nicolai@outlook.es>
2021-03-06 22:09:12 -0600

    NavigationView para profesor agregado

--- app/src/main/java/com/example/prototipo2tt/profesor_home.java
+++ app/src/main/java/com/example/prototipo2tt/profesor_home.java
@@ -1,14 +1,71 @@
 package com.example.prototipo2tt;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class profesor_home extends AppCompatActivity {
-
+public class profesor_home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout dl;
+    NavigationView nv;
+    Toolbar toolb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profesor_home);
+        dl = (DrawerLayout) findViewById(R.id.drawer_layout_profesor);
+        nv = (NavigationView) findViewById(R.id.nav_view_profesor);
+        toolb = (Toolbar) findViewById(R.id.toolbar_profesor);
 
+        setSupportActionBar(toolb);
+
+        //hide and show items
+        Menu menu = nv.getMenu();
+        menu.findItem(R.id.id_menuloginprofesor).setVisible(false);
+
+        nv.bringToFront();
+        ActionBarDrawerToggle tg = new ActionBarDrawerToggle(this, dl, toolb, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        dl.addDrawerListener(tg);
+        tg.syncState();
+
+        nv.setNavigationItemSelectedListener(this);
+        nv.setCheckedItem(R.id.id_menuhorarioprofesor);
+
+    }
+    @Override
+    public void onBackPressed(){
+        if(dl.isDrawerOpen(GravityCompat.START)){
+            dl.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.id_menuhorarioprofesor:
+                break;
+            case R.id.id_menubuzon:
+                /*
+                Intent i = new Intent(MainActivity.this, Bus.class);
+                startActivity(i);
+                */
+                break;
+            case R.id.id_menubitacora:
+                Toast.makeText(this, "Nada que mostrar", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        dl.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
